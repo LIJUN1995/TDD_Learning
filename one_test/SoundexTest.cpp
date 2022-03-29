fe2ba5e87c37219ca5c0a93a0e3a20eed439d179
jun.li <1340389866@qq.com>
2022-03-29 17:44:35 +0800

    add an assertion

--- one_test/SoundexTest.cpp
+++ one_test/SoundexTest.cpp
@@ -1,8 +1,19 @@
-class Soundex {
+#include <string>
+
+class Soundex
+{
+public:
+    std::string encode(const std::string& word) const {
+        return word;
+    }
 };
 
 #include "gmock/gmock.h"
 
 TEST(SoundexEncoding, RetainSoleLetterOfOneLetterWord) {
     Soundex soundex;
+
+    auto encoded = soundex.encode("A");
+
+    ASSERT_THAT(encoded, testing::Eq("A"));
 }
