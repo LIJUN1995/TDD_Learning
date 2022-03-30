ac44f50a6b9818d528de4a008bc162d6ff3cf62d
jun.li <1340389866@qq.com>
2022-03-30 20:21:41 +0800

    jump out of the rule to test

--- one_test/Soundex.h
+++ one_test/Soundex.h
@@ -5,10 +5,8 @@
 
 class Soundex {
 public:
-    static const size_t MaxCodeLength{4};
-
     std::string encode(const std::string& word) const {
-        return zeroPad(head(word) + encodedDigits(tail(word)));
+        return zeroPad(upperFront(head(word)) + encodedDigits(tail(word)));
     }
 
     std::string encodedDigit(char letter) const {
@@ -20,12 +18,15 @@ public:
             {'n', "5"}, {'r', "6"},
         };
 
-        auto it = encodings.find(letter);
+        auto it = encodings.find(lower(letter));
 
-        return it == encodings.end() ? "" : it->second;
+        return it == encodings.end() ? NotADigit : it->second;
     }
 
 private:
+    static const size_t MaxCodeLength{4};
+    const std::string NotADigit{"*"};
+
     std::string  zeroPad(const std::string& word) const {
         auto zerosNeeded = MaxCodeLength - word.length();
 
@@ -41,8 +42,9 @@ private:
         for (auto letter: word) {
             if (isComplete(encoding))
                 break;
-            if (encodedDigit(letter) != lastDigit(encoding))
-                encoding += encodedDigit(letter);   
+            auto digit = encodedDigit(letter);
+            if (digit != NotADigit && digit != lastDigit(encoding))
+                encoding += digit;   
         }
             
         return encoding;
@@ -58,8 +60,17 @@ private:
 
     std::string lastDigit(const std::string& encoding) const {
         if (encoding.empty())
-            return "";
+            return NotADigit;
+
         return std::string(1, encoding.back());
+    }
+
+    std::string upperFront(const std::string& string) const {
+        return std::string(1, std::toupper(static_cast<unsigned char>(string.front())));
+    }
+
+    char lower(char c) const {
+        return std::tolower(static_cast<unsigned char>(c));
     }
 };
 
