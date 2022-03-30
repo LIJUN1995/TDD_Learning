49be84073445218c932072710525a50b667f569a
jun.li <1340389866@qq.com>
2022-03-30 20:42:50 +0800

    not case sensitive

--- one_test/Soundex.h
+++ one_test/Soundex.h
@@ -6,7 +6,7 @@
 class Soundex {
 public:
     std::string encode(const std::string& word) const {
-        return zeroPad(upperFront(head(word)) + encodedDigits(tail(word)));
+        return zeroPad(upperFront(head(word)) + tail(encodedDigits(word)));
     }
 
     std::string encodedDigit(char letter) const {
@@ -39,7 +39,10 @@ private:
 
     std::string encodedDigits(const std::string& word) const {
         std::string encoding;
-        for (auto letter: word) {
+
+        encoding += encodedDigit(word.front());
+
+        for (auto letter: tail(word)) {
             if (isComplete(encoding))
                 break;
             auto digit = encodedDigit(letter);
@@ -55,7 +58,7 @@ private:
     }
 
     bool isComplete (const std::string& encoding) const {
-        return encoding.length() == MaxCodeLength - 1;
+        return encoding.length() == MaxCodeLength;
     }
 
     std::string lastDigit(const std::string& encoding) const {
